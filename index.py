e11c282c953af8df0218b052c55dd8d69a9082db
GQSM <ms314006@hotmail.com>
2019-06-05 08:37:10 +0800

    [ADD] get news route

--- index.py
+++ index.py
@@ -13,7 +13,26 @@ def index():
     resp = make_response(strJson) 
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+@app.route("/news/it")
+def getNewsIt():
+    req = requests.request('GET', 'https://tw.news.yahoo.com/tech-development',headers=headers)
+    soup = BeautifulSoup(req.text)
+    titleDom = soup.find_all('h3', class_='Mb(5px)')
+    strJson = '{"data":['
+    for title in titleDom:
+        strJson += '{"title":"' + title.text + '",'
+        contentReq = requests.request('GET', 'https://tw.news.yahoo.com/' + title.find('a')['href'],headers=headers)
+        contentSoup = BeautifulSoup(contentReq.text)
+        contentDom = contentSoup.find_all('p', class_='canvas-atom canvas-text Mb(1.0em) Mb(0)--sm Mt(0.8em)--sm')
+        strJson += '"content":"'
+        for content in contentDom:
+            strJson += content.text
+        strJson += '"},'
+    strJson = strJson[:-1] + ']}'
 
+    resp = make_response(strJson) 
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
