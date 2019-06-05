98933c7f58db04088cc5cde7d4195e799bd22d5d
GQSM <ms314006@hotmail.com>
2019-06-05 09:07:47 +0800

    [CHANGE] 移除掉多餘的 index route

--- index.py
+++ index.py
@@ -6,13 +6,6 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
 
-@app.route("/index")
-def index():
-    strJson += 'Hi'
-
-    resp = make_response(strJson) 
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
 @app.route("/news/it")
 def getNewsIt():
     req = requests.request('GET', 'https://tw.news.yahoo.com/tech-development',headers=headers)
